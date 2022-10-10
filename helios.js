907b3aeee6d2e6510b762f9c6fa7d32ba6ceafc8
Christian Schmitz <cschmitz398@gmail.com>
2022-10-09 23:00:46 -0500

    version bump

--- helios.js
+++ helios.js
@@ -6,7 +6,7 @@
 // Author:      Christian Schmitz
 // Email:       cschmitz398@gmail.com
 // Website:     github.com/hyperion-bt/helios
-// Version:     0.6.8
+// Version:     0.6.9
 // Last update: October 2022
 // License:     Unlicense
 //
@@ -201,7 +201,7 @@
 // Section 1: Global constants and vars
 ///////////////////////////////////////
 
-export const VERSION = "0.6.8"; // don't forget to change to version number at the top of this file, and in package.json
+export const VERSION = "0.6.9"; // don't forget to change to version number at the top of this file, and in package.json
 
 var DEBUG = false;
 
