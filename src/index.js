33c4620b1067871bcb09197ede1398e03de3d0da
Christian Schmitz <cschmitz398@gmail.com>
2024-04-01 14:09:11 -0600

    wrapped @hyperionbt/helios v0.6.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 export * from "@hyperionbt/helios"
+
+import { Program } from "@hyperionbt/helios"
+
+/**
+ * @typedef {{
+ *   optimize?: boolean
+ * }} CompileOptions
+ */
+
+/**
+ * @param {string} main 
+ * @param {CompileOptions} options 
+ * @returns {string} - cborHex of UplcProgram (UplcProgramV2)
+ */
+export function compile(main, options = {}) {
+    const optimize = options.optimize ?? false
+
+    const program = Program.new(main)
+
+    return JSON.parse(program.compile(optimize).serialize()).cborHex
+}
