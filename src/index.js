cd1bab85f21010edaa7a9358c970059ae62ccef9
Christian Schmitz <cschmitz398@gmail.com>
2024-04-01 15:03:45 -0600

    wrapped @hyperionbt/helios v0.8.9

--- src/index.js
+++ src/index.js
@@ -1,22 +1,127 @@
 export * from "@hyperionbt/helios"
 
 import { Program } from "@hyperionbt/helios"
+import { getShebang } from "typescript"
+
+/**
+ * @typedef {{
+ * }} TypeCheckOptions
+ */
+
+
+/**
+ * `ref` can be used for recursive types (eg. Trees)
+ * @typedef {{
+*     ref: string
+* } | {
+*     kind: "internal"
+*     internal: string
+*     id?: string
+* } | {
+*     kind: "list"
+*     item: TypeSchema
+*     id?: string
+* } | {
+*     kind: "map"
+*     key: TypeSchema
+*     value: TypeSchema
+*     id?: string
+* } | {
+*     kind: "option"
+*     some: TypeSchema
+*     id?: string
+* } | {
+*     kind: "struct"
+*     fields: {
+*         name: string
+*         type: TypeSchema
+*     }[]
+*     id?: string
+* } | {
+*     kind: "enum"
+*     variants: {
+*          name: string,
+*          fieldTypes: {
+*              name: string
+*              type: TypeSchema
+*          }[]
+*     }[]
+*     id?: string
+* }} TypeSchema
+*/
+
+/**
+ * @typedef {{
+ *   arguments?: TypeSchema[]
+ *   parameters?: {
+ *     [name: string]: TypeSchema
+ *   }
+ * }} TypeCheckOutput
+ */
+
+/**
+ * @param {string} main
+ * @param {string[]} modules
+ * @param {TypeCheckOptions} options
+ * @returns {TypeCheckOutput}
+ */
+export function check(main, modules = [], options = {}) {
+    const program = Program.new(main, modules)
+
+    /**
+     * @param {any} type 
+     * @returns {TypeSchema}
+     */
+    function getSchema(type) {
+        if (type.path.includes("scriptcontext")) {
+            return {kind: "internal", internal: "ScriptContext"}
+        } else {
+            return {kind: "internal", internal: "Data"}
+        }
+    }
+
+    const paramTypes = program.paramTypes
+
+    return {
+        arguments: program.mainFunc.argTypes.map(at => getSchema(at.path)),
+        parameters: Object.fromEntries(Object.keys(paramTypes).map(k => [k, getSchema(paramTypes[k])]))
+    }
+}
 
 /**
  * @typedef {{
  *   optimize?: boolean
+ *   modules?: string[]
+ *   parameters?: {
+ *     [name: string]: any
+ *   }
  * }} CompileOptions
+ */
+
+/**
+ * arguments: 2 (redeemer and ScriptContext) or 3 (datum, redeemer and ScriptContext)
+ * @typedef {{
+ *   type: string
+ *   description?: string
+ *   cborHex: string
+ * }} CompileOutput
  */
 
 /**
  * @param {string} main 
  * @param {CompileOptions} options 
- * @returns {string} - cborHex of UplcProgram (UplcProgramV2)
+ * @returns {CompileOutput}
  */
 export function compile(main, options = {}) {
     const optimize = options.optimize ?? false
 
-    const program = Program.new(main)
+    const program = Program.new(main, options?.modules ?? [])
 
-    return JSON.parse(program.compile(optimize).serialize()).cborHex
+    const params = options?.parameters ?? {}
+    
+    for (let k in params) {
+        program.changeParam(k, JSON.stringify(params[k]))
+    }
+
+    return JSON.parse(program.compile(optimize).serialize())
 }
