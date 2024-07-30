698a6bfbca0cf86c7f347e6588c996b8378c7304
Christian Schmitz <cschmitz398@gmail.com>
2024-07-30 11:43:36 -0600

    - VERSION exported through src/index.js - VERSION check included in package.json

--- src/program/index.js
+++ src/program/index.js
@@ -1,5 +1,6 @@
 export { newEntryPoint } from "./newEntryPoint.js"
 export { Program } from "./Program.js"
+export { VERSION } from "./version.js"
 
 /**
  * @typedef {import("./Program.js").ProgramProps} ProgramProps
---
+++ src/program/version.js
@@ -0,0 +1,1 @@
+export const VERSION = "0.17.0-1"
